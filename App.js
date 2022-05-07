0973caf99fb803fe96f96fb48f0336074744bfed
sachitha kariyawasam <klgsachitha@gmail.com>
2022-05-07 14:10:43 +0530

    MIT3206-T21-A3

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-start",
         alignItems: "center",
+        padding: 40,
       }}
     >
-      <Text>Universal React with Expo</Text>
+      <TouchableOpacity
+        style={{
+          alignItems: "center",
+          backgroundColor: "#DDDDDD",
+          padding: 100,
+        }}
+      >
+        <Text>UCSC</Text>
+        <Text>2022</Text>
+        <Text>ආයුබෝවන්</Text>
+        <Text>**</Text>
+        <Text>MIT3206</Text>
+        <Text>AND</Text>
+        <Text>MIT3106</Text>
+      </TouchableOpacity>
     </View>
   );
 }
