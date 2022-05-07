e2c7a8ea11a054f3e0f787118b464e1e8e20f31a
sachitha kariyawasam <klgsachitha@gmail.com>
2022-05-07 13:12:06 +0530

    MIT3206-A1

--- App.js
+++ App.js
@@ -9,7 +9,9 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text>Universal React with Expo</Text>
+      <Text>Name: G.H.P.S.Vimukthi</Text>
+      <Text>Index No: 19550962</Text>
+      <Text>Email: swtutu@gmail.com</Text>
     </View>
   );
 }
